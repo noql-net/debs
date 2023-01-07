8bfeb6e45683e479137a74c5012b424d449dd1bd
Mark Pashmfouroush <mark@markpash.me>
2023-01-07 03:53:26 +0000

    xray-core 1.7.1
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- packages/xray-core.nix
+++ packages/xray-core.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "xray-core";
-  version = "1.7.0";
+  version = "1.7.1";
 
   src = fetchFromGitHub {
     owner = "XTLS";
     repo = "Xray-core";
     rev = "v${version}";
-    sha256 = "sha256-aNIb90obew+tvd/rYVddK1MmUqLXSqi9xAu65BQFvk0=";
+    sha256 = "sha256-mhaYlQ4tq6Al5D8+pzgwqyaTEHOG2AgzuQKYTRZ88Ts=";
   };
 
   CGO_ENABLED = 0;
 
-  vendorSha256 = "sha256-zZdUPjlul9f1jKy0Zf79KOToHIsvfgwDp6XpFPYyTzk=";
+  vendorSha256 = "sha256-P2g0MqlBScm6yTnpvL5T6l9ntsb4tK9k3Civ7rTevrE=";
 
   ldflags = [ "-s" "-w" "-buildid=" ];
   subPackages = [ "main" ];
