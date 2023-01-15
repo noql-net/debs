c46f423f091674052c6648439f7bac66a1665dd4
Mark Pashmfouroush <mark@markpash.me>
2023-01-15 11:32:18 +0000

    create script to update readme with package list
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- toolbox/shell.nix
+++ toolbox/shell.nix
@@ -21,11 +21,24 @@ let
     runtimeInputs = [ pkgs.aptly ];
     text = ''aptly publish update -force-overwrite all s3:noql-apt:all'';
   };
+
+  readme-update = pkgs.writeShellApplication {
+    name = "readme-update";
+    runtimeInputs = [ pkgs.gomplate ];
+    text = ''
+      nix eval --json '.#packages.x86_64-linux' --apply builtins.attrNames | \
+      jq 'map(select(.|test("(deb|default)$")|not))' | \
+      gomplate --datasource pkgs=stdin:///pkgs.json --file readme.tmpl --out README.md
+    '';
+  };
 in pkgs.mkShell {
   buildInputs = [
     pkgs.aptly
+    pkgs.gomplate
+
     build-debs
     repo-add
     repo-publish
+    readme-update
   ];
 }
