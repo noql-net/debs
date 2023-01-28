1c9c220f367fb12a5a092b81b77008ee01344e0c
Mark Pashmfouroush <mark@markpash.me>
2023-01-28 11:58:27 +0000

    chisel 1.8.1
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- packages/chisel.nix
+++ packages/chisel.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "chisel";
-  version = "1.7.7";
+  version = "1.8.1";
 
   src = fetchFromGitHub {
     owner = "jpillora";
     repo = "chisel";
     rev = "v${version}";
-    sha256 = "sha256-3EaVUGcwkJWX0FxIaHddUehJIdbxAPfBm8esXKCUuhM=";
+    sha256 = "sha256-N2voSclNH7lGbUkZo2gkrEb6XoA5f0BzNgAzQs1lOKQ=";
   };
 
   CGO_ENABLED = 0;
 
-  vendorSha256 = "sha256-Oko9nduKW76NIUCVyF0lPzEH+TFT1el9VGIbm5lQXtM=";
+  vendorSha256 = "sha256-p/5g4DLoUhEPFBtAbMiIgc6O4eAfbiqBjCqYkyUHy70=";
 
   ldflags = [
     "-s"
