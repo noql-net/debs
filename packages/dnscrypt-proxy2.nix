1e66a0f4d9c7e362bf1da3ba50947c55b392c5c7
Mark Pashmfouroush <mark@markpash.me>
2023-02-02 23:28:14 +0000

    dnscrypt-proxy2 2.1.3
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- packages/dnscrypt-proxy2.nix
+++ packages/dnscrypt-proxy2.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "dnscrypt-proxy2";
-  version = "2.1.2";
+  version = "2.1.3";
 
   src = fetchFromGitHub {
     owner = "DNSCrypt";
     repo = "dnscrypt-proxy";
     rev = "${version}";
-    sha256 = "sha256-vFUbsgLgwQGb/2rValFRuZXguxyAJzOBMLnbVs0swes=";
+    sha256 = "sha256-5wfxjx8SxynY6DpPIvOLwSsBdM/0zSzfaVDQTI/RUD0=";
   };
 
   CGO_ENABLED = 0;
