f0632cd0fd24c4981ea4d42d0b2da108efa16494
Mark Pashmfouroush <mark@markpash.me>
2023-02-02 17:51:34 +0000

    gost 3.0.0-rc5
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- packages/gost.nix
+++ packages/gost.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "gost";
-  version = "3.0.0-rc.4";
+  version = "3.0.0-rc5";
 
   src = fetchFromGitHub {
     owner = "go-gost";
     repo = "gost";
     rev = "v${version}";
-    sha256 = "sha256-40nogMDDlZk11560BDZg9Glv3L9DqGx8QL9z034yYrk=";
+    sha256 = "sha256-1vrBBGu6cWlTQbRVZeY5WHQ77Y9yL7+EYOW56QH6+Xg=";
   };
 
   CGO_ENABLED = 0;
 
-  vendorSha256 = "sha256-3/+qh81P1kcjHd4kGOVf6rztM12wcLHhcIz5zx6l5Bk=";
+  vendorSha256 = "sha256-seca9P7wDtZbKSo/l44ySJk8drFOkTSeuiSlwDgQ9zk=";
 
   ldflags = [ "-s" "-w" "-buildid=" ];
   subPackages = [ "cmd/gost" ];
