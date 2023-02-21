2c7dad6f06a5d607c4f77b6fe769b8e1576ae247
Mark Pashmfouroush <mark@markpash.me>
2023-02-21 13:51:25 +0000

    shadowsocks-v2ray-plugin 5.4.0
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- packages/shadowsocks-v2ray-plugin.nix
+++ packages/shadowsocks-v2ray-plugin.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "shadowsocks-v2ray-plugin";
-  version = "5.3.0";
+  version = "5.4.0";
 
   src = fetchFromGitHub {
     owner = "teddysun";
     repo = "v2ray-plugin";
     rev = "v${version}";
-    sha256 = "sha256-FlBbQmovxEP/7dEP99WPHz+2mr4MqeNvpP+th7Ov/y0=";
+    sha256 = "sha256-QTuyMMjctnimsKxtkVjIjafQLXYc08D6UX+lwNsT6MI=";
   };
 
   CGO_ENABLED = 0;
 
-  vendorSha256 = "sha256-dc4BosO40tIITfa8gn7Aq244h/9NsAdp3TmevmEabVo=";
+  vendorSha256 = "sha256-MBjEM2S2G+8vfFmNqCsW5SRPvX6eo9rEuluvCPln4EQ=";
 
   ldflags = [ "-s" "-w" "-buildid=" ];
 
