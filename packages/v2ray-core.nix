2172e2ae999323298adae644554c9793b3610e41
Mark Pashmfouroush <mark@markpash.me>
2023-02-21 13:48:36 +0000

    v2ray-core 5.4.0
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- packages/v2ray-core.nix
+++ packages/v2ray-core.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "v2ray-core";
-  version = "5.3.0";
+  version = "5.4.0";
 
   src = fetchFromGitHub {
     owner = "v2fly";
     repo = "v2ray-core";
     rev = "v${version}";
-    hash = "sha256-LLvAoPA3rLGfhrWD37OVyWd4tC9mwgpE1WjK2pY27xU=";
+    hash = "sha256-dgWpfpJiPYQmVg7CHRE8f9hX5vgC2TuLpTfMAksDurs=";
   };
 
   CGO_ENABLED = 0;
 
-  vendorSha256 = "sha256-xBNVyPN9PRkraPRzZXOguGemYPm+Jw8hl04vWV8TZaA=";
+  vendorSha256 = "sha256-BEMdh1zQdjVEu0GJt6KJyWN5P9cUHfs04iNZWxzZ0Yo=";
 
   ldflags = [ "-s" "-w" "-buildid=" ];
   subPackages = [ "main" ];
